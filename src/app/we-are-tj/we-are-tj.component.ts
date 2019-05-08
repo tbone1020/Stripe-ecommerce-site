472cf25ba318bb7f12462306bea73c750e0ca039
tbone1020 <trentteebken@trents-mbp.home>
2019-05-07 22:37:14 -0700

    Added Angular components

---
+++ src/app/we-are-tj/we-are-tj.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-we-are-tj',
+  templateUrl: './we-are-tj.component.html',
+  styleUrls: ['./we-are-tj.component.css']
+})
+export class WeAreTjComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
