5551451640416d09c5570a9d2ef9e1863730b25b
tbone1020 <trentteebken@trents-mbp.home>
2019-05-09 23:09:46 -0700

    Building front end for We are Travel japan

--- src/app/special-offers/special-offers.component.ts
+++ src/app/special-offers/special-offers.component.ts
@@ -7,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialOffersComponent implements OnInit {
 
-  constructor() { }
+  specialOffers: Object[];
+
+  constructor() {
+    this.specialOffers = [{
+      image: "shinjuku-thumbnail.png",
+      description: "9 Nights including, Hotel and Airfare, in Tokyo's famous Shinjuku district",
+      location: "Tokyo",
+      price: "$2,500.00"
+    },{
+      image: "osaka-thumbnail.png",
+      description: "9 nights in a 5 start hotel in Osaka on the Dotonbori River.",
+      location: "Osaka",
+      price: "$1,500.00"
+    },{
+      image: "kyoto-tori-thumbnail.png",
+      description: "14 days in Kyoto's historic district.",
+      location: "Kyoto",
+      price: "$3,000.00"      
+    }]
+  }
 
   ngOnInit() {
   }
--- src/app/we-are-tj/we-are-tj.component.ts
+++ src/app/we-are-tj/we-are-tj.component.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeAreTjComponent implements OnInit {
 
-  constructor() { }
+  weAreJapanText: Object[];
+
+  constructor() {
+    this.weAreJapanText = [{
+      icon: "fas fa-train",
+      header: "Japan Rail Pass",
+      text: "We sell passes for the famous high speed bullet train for 7 days, 14 days, or 21 days for coach and 1st class."
+    }, {
+      icon: "fas fa-ticket-alt",
+      header: "Subway Train Passes",
+      text: "Skip the long lines in the subways stations. Buy a day pass or a week pass for either Tokyo, Osaka, Kyoto, or Hiroshima."
+    }, {
+      icon: "fab fa-telegram-plane",
+      header: "Airfaire deals",
+      text: "The most expensive part of traveling can be Airfere. Don't worry, we got your back. We bundled cheap airfare with hotels along tours so you're saving and have enough for the delicious Ramen."
+    }];
+  }
 
   ngOnInit() {
   }
